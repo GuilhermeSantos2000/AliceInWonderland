a0c3f65a7524da4b3d33b39d66199754297c5b8b
GuilhermeSantos2000 <guilhermesantos.gamedev@gmail.com>
2025-05-16 14:16:08 +0100

    Platform Respawn

--- platformer-main/Assets/scripts/PlatformBreak.cs
+++ platformer-main/Assets/scripts/PlatformBreak.cs
@@ -4,6 +4,7 @@ public class PlatformBreak : MonoBehaviour
 {
     [SerializeField] private string triggeringTag = "Player"; // Tag of the object that can trigger the destruction
     [SerializeField] private float destructionDelay = 2f;     // Delay before destruction in seconds
+    [SerializeField] private float respawnDelay = 5f;     // Delay before respawn in seconds
     [SerializeField] private Animator animator;         // Animator component for the platform
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -14,9 +15,12 @@ public class PlatformBreak : MonoBehaviour
             // Check if the collision is coming from above
             if (collision.contacts[0].normal.y < -0.5f) // Normal.y < -0.5f indicates collision from above
             {
-                animator.SetTrigger("BreakThis"); // Trigger the break animation
+                // Trigger the break animation
+                animator.SetTrigger("BreakThis");
                 // Start the destruction process with a delay
                 Invoke(nameof(DestroyPlatform), destructionDelay);
+                // Start the respawn process with a delay
+                Invoke(nameof(RestorePlatform), respawnDelay);
             }
         }
     }
@@ -24,5 +28,13 @@ public class PlatformBreak : MonoBehaviour
     private void DestroyPlatform()
     {
         gameObject.SetActive(false); // Deactivate the platform
+    }
+
+    private void RestorePlatform()
+    {
+        // Restore the idle animation
+        animator.SetTrigger("Respawn");
+
+        gameObject.SetActive(true); // Deactivate the platform
     }
 }
